94093066dadaf71599a7f2092d0a7c4139003e85
Madeline Stevens <stevensm1987@gmail.com>
2017-11-12 00:51:12 -0800

    desperate attempt changes

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(`${__dirname}/build`));
-app.get('*', (req, res)=> res.sendFile(`${__dirname}/build/index.html`));
+app.get('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`));
 
 app.listen(process.env.PORT, () => console.log(`SERVER RUNNING ON: ${process.env.PORT}`));
